e4f96db067315c02a4dc721f90b2d1735140f927
JuhyeokSin <juhyeok.sin@sjsu.edu>
2019-10-15 22:00:01 -0700

    1st draft of the Preprocess class

--- Preprocess.py
+++ Preprocess.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 
 
 class Preprocess:
+
+    # initialize a newly created object
     def __init__(self, path):
         self.path = path
     
@@ -37,16 +39,7 @@ class Preprocess:
         return train
 
 
-    def display_img_HU(pixel_img, window_center, window_width, intercept, slope):
-        """
-        displays a image transformed into Hounsefield Units from pixel values
-        """
-        HU_img = window_image(pixel_img, window_center, window_width, intercept, slope)
-        plt.imshow(HU_img, cmap=plt.cm.bone)
-        plt.grid(False)
-
-
-    def window_image(pixel_array, window_center, window_width, intercept, slope):
+    def window_image(self, pixel_array, window_center, window_width, intercept, slope):
         """
         Transform the pixel values to Hounsefield units (HU)
         pixel_array = one DICOM image converted into 2-D pixel array
@@ -68,9 +61,18 @@ class Preprocess:
         pixel_array_HU[pixel_array_HU>pixel_array_HU_max] = pixel_array_HU_max
             
         return pixel_array_HU
+        
+
+    def display_img_HU(self, pixel_img, window_center, window_width, intercept, slope):
+        """
+        displays a image transformed into Hounsefield Units from pixel values
+        """
+        HU_img = window_image(pixel_img, window_center, window_width, intercept, slope)
+        plt.imshow(HU_img, cmap=plt.cm.bone)
+        plt.grid(False)
     
 
-    def get_first_of_dicom_field_as_int(x):
+    def get_first_of_dicom_field_as_int(self, x):
         """
         get x[0] as in int is x is a 'pydicom.multival.MultiValue', otherwise get int(x)
         """
@@ -80,7 +82,7 @@ class Preprocess:
             return int(x)
 
 
-    def get_windowing(metadata):
+    def get_windowing(self, metadata):
         """
         Get window setting metadata among the metametadata out of a DICOM image
         """
@@ -92,14 +94,19 @@ class Preprocess:
         return [get_first_of_dicom_field_as_int(x) for x in dicom_fields]
     
 
-    def pixel_to_hounsefield(pixel_array, metadata):
+    def pixel_to_hounsefield(self, pixel_array, metadata):
         return window_image(pixel_array, get_windowing(metadata))
 
-    def decimalize(image_HU):
+    
+    def decimalize(self, image_HU):
         """
         Convert values of Housefield Units into a range of (0, 1)
         image_HU: an image in Hounsefield Unit that outliers removed
         """
         min = np.amin(image_HU)
         max = np.amax(image_HU)
-        array_HU_shifted = image_HU - min
+        array_HU_shifted = image_HU - min # array_HU is shifted by min
+
+        array_HU_decimalized = array_HU_shifted / (max - min)
+        return array_HU_decimalized
+    
